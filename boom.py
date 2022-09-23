f475b81976ab99e381a872b1fa3f1e9cff9d7506
Vincent Boyer <boyer.vincent@gmail.com>
2022-09-23 11:07:33 +0200

    Update boom.py

--- boom.py
+++ boom.py
@@ -1,2 +1,2 @@
- database = aws_lib.connect("AKIAF6BACLKR45SBWSZ0", "hjshnk5ex5u34565AWS604/JKGjhz545d89sjkje")
+ database = aws_lib.connect("AKIAF6BACLKR45BBWSZ0", "hjshnk5ex5u34565AWS604/JKGjhz545d89sjkje")
 # No newline at end of file
