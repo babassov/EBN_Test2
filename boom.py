daca1c86255e0e8cbc6074859a731a539cc9f59b
Vincent Boyer <boyer.vincent@gmail.com>
2022-09-30 14:19:09 +0200

    Update boom.py

--- boom.py
+++ boom.py
@@ -1,6 +1,8 @@
  database = aws_lib.connect("AKIAF6BACLKR45BBWSZ0", "hjshnk5ex5u34565AWS604/JKGjhz545d89sjkje")
 # No newline at end of file
 
-test_keyword_eric_3243
+var pg_port=1212;
+var pg_host="gitguardianst.com:9082/BLUDB";
+var pg_user="root";
+var pg_pass="sup3rstr0ngpass1ForGHH";
 
-gg-business-demo
