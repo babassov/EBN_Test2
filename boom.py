622fee491a8c3f88a787cf93e000e052e7ebb079
Vincent Boyer <boyer.vincent@gmail.com>
2022-10-10 11:42:14 +0200

    Update boom.py

--- boom.py
+++ boom.py
@@ -6,3 +6,5 @@ var pg_host="gitguardianst.com:9082/BLUDB";
 var pg_user="root";
 var pg_pass="sup3rstr0ngpass1ForGHH";
 
+
+  database = aws_lib.connect("AKIAF6BAFJKL45SBWLY0", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
