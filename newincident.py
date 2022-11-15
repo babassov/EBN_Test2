3589c00502eb6bc4fcad44f1b5fdf3f86b0b26e5
Vincent Boyer <boyer.vincent@gmail.com>
2022-11-15 10:07:15 +0100

    Update newincident.py

--- newincident.py
+++ newincident.py
@@ -1,1 +1,2 @@
  database = aws_lib.connect("AKIAF6BAFJKR45SBWZZ0", "hjshnk5ex5u34565AWS654/JKGjhz595d89sjkje")
+ database = aws_lib.connect("AKIAF6BADJKR45SBWSZ0", "hjshnk5ex5u34965AWS654/JKGjhz545d89sjkje")
