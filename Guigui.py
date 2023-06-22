6f3dfaca92124827e11324bc5ecbbda242a54c4f
Vincent Boyer <boyer.vincent@gmail.com>
2023-06-22 16:13:27 +0200

    Update Guigui.py

--- Guigui.py
+++ Guigui.py
@@ -1,2 +1,9 @@
 vbkeyword012023 15h01 new repo
 vbkeyword012023 15h07
+
+
+function post(el, cmd) {
+    try {
+        el.contentWindow.postMessage(JSON.stringify(assign({event: 'command'}, cmd)), '*');
+    } catch (e) {}
+}
