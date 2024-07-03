61d15d800c25c5d6c21c463b06ec86e2a8973849
Vincent Boyer <26900412+babassov@users.noreply.github.com>
2024-07-03 19:14:57 +0200

    Update boom.py

--- boom.py
+++ boom.py
@@ -10,3 +10,7 @@ var pg_pass="sup3rstr0ngpass1ForGHH";
   database = aws_lib.connect("AKIAF6BAFJKL45SBWLY0", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
    database = aws_lib.connect("AKIAF6BAFJKL45SBWLY0", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkje")
 
+
+
+
+ database = aws_lib.connect("AKIAF6BAFJSR45SBWSZ0", "hjshnk5ex5u34565AWS654/MKGjhz545d89sjkje")
